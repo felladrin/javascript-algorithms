3c525bb4261ccffd38531a6230248fe4352f0b37
Victor Nogueira <felladrin@gmail.com>
2017-03-18 03:32:39 -0300

    Rewrite deque class without using array's functions

--- data-structures/deque/deque.js
+++ data-structures/deque/deque.js
@@ -1,62 +1,99 @@
 function Deque() {
+    this.length = 0;
     this.items = [];
 }
 
 /**
- * @returns {object} item
+ * @returns {object} item The deleted item, which was previous at the beginning of the deque.
  */
 Deque.prototype.dequeueFront = function () {
-    return this.items.splice(0, 1)[0];
+    var deletedFront = this.front();
+    var reindexItems = [];
+    for (var i = 1; i < this.length; i++) {
+        reindexItems[i - 1] = this.items[i];
+    }
+    this.items = reindexItems;
+    this.length--;
+    return deletedFront;
 };
 
 /**
- * @param {object} item
+ * @param {object} item The item to be put at the beginning of the deque.
  */
 Deque.prototype.enqueueFront = function (item) {
-    this.items.unshift(item);
+    var reindexItems = [];
+    reindexItems[0] = item;
+    for (var i = 0; i < this.length; i++) {
+        reindexItems[i + 1] = this.items[i];
+    }
+    this.items = reindexItems;
+    this.length++;
 };
 
 /**
- * @returns {object} item
+ * @returns {object} item The deleted item, which was previous at the end of the deque.
  */
 Deque.prototype.dequeueBack = function () {
-    return this.items.splice(this.items.length-1)[0];
+    var deletedBack = this.back();
+    var reindexItems = [];
+    for (var i = 0; i < (this.length - 1); i++) {
+        reindexItems[i] = this.items[i];
+    }
+    this.items = reindexItems;
+    this.length--;
+    return deletedBack;
 };
 
 /**
- * @param {object} item
+ * @param {object} item The item to be put at the end of the deque.
  */
 Deque.prototype.enqueueBack = function (item) {
-    this.items[this.items.length] = item;
+    this.items[this.length++] = item;
 };
 
 
 /**
- * @returns {object} item
+ * @returns {object} item The item which is at the beginning of the deque.
  */
 Deque.prototype.front = function () {
     return this.items[0];
 };
 
 /**
- * @returns {object} item
+ * @returns {object} item The item which is at the end of the deque.
  */
 Deque.prototype.back = function () {
-    return this.items[this.items.length-1];
+    return this.items[this.length - 1];
 };
 
 /**
- * @returns {number}
+ * @returns {number} The current number of items in the deque.
  */
 Deque.prototype.size = function () {
-    return this.items.length;
+    return this.length;
 };
 
 /**
- * @returns {boolean}
+ * @returns {boolean} Whether the deque is empty or not.
  */
 Deque.prototype.isEmpty = function () {
-    return this.items.length === 0;
+    return (this.length === 0);
+};
+
+/**
+ * @returns {string} The string representation of the deque.
+ */
+Deque.prototype.toString = function () {
+    var result = '[';
+    var lastItemKey = this.length - 1;
+    for (var i = 0; i < lastItemKey; i++) {
+        result += this.items[i] + ', ';
+    }
+    if (this.items[lastItemKey]) {
+        result += this.items[lastItemKey];
+    }
+    result += ']';
+    return result;
 };
 
 module.exports = Deque;
