9671f6165c8870560fe3734dcb88a08bbd055a1c
Victor Nogueira <felladrin@gmail.com>
2017-04-01 19:17:02 -0300

    Use ES6 string concatenation and `for...of`

--- test/sorting.js
+++ test/sorting.js
@@ -4,14 +4,14 @@ const assert = require('chai').assert;
 const JavascriptSort = require('../sorting/javascript_sort');
 const getRandomIntArray = require('../helpers/get_random_int_array');
 const cloneArray = require('../helpers/clone_array');
-const sortingAlgorithms = require('require-all')({dirname: __dirname + '/../sorting'});
+const sortingAlgorithms = require('require-all')({dirname: `${__dirname}/../sorting`});
 
 Object.keys(sortingAlgorithms).forEach(key => {
     let sortingAlgorithm = sortingAlgorithms[key];
     describe(sortingAlgorithm.name, () => {
-        getRandomIntArray(5, 8, 64).forEach(length => {
+        for (const length of getRandomIntArray(5, 8, 64)) {
             let vector = getRandomIntArray(length, 1, 100);
-            it("should correctly sort " + vector.toString(), () => {
+            it(`should correctly sort ${vector.toString()}`, () => {
                 let expected = cloneArray(vector);
                 JavascriptSort(expected);
 
@@ -20,7 +20,6 @@ Object.keys(sortingAlgorithms).forEach(key => {
 
                 assert.deepEqual(expected, actual);
             });
-        });
+        }
     });
 });
-
